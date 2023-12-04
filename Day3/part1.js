e605778c112f9aebf49ecc5a38fae5c008f3cb06
AmbitionSZN <jahlwalk22635@gmail.com>
2023-12-04 18:10:12 -0500

    finished day 3 part 1

--- Day3/part1.js
+++ Day3/part1.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const fs = require('fs');
 const data = fs.readFileSync('puzzleinput.txt', 'utf8', (err, data) => {
     if (err) {
@@ -12,7 +13,7 @@ for (let i = 0; i < arrData.length; i++) {
     arrData[i] = arrData[i].split('');
 }
 
-console.log(arrData);
+//console.log(arrData);
 const numArr = []
 const newNumArr = []
 
@@ -23,17 +24,89 @@ for (let i = 0; i < arrData.length; i++) {
 
       }
     }
-  console.log(numArr)
 }
-
-for (let i = 0; i < numArr[i].length; i++) {
-  newNumArr.push(numArr[i])
-  if (numArr[i][1] === numArr[i][1] - 1) {
-    newNumArr.push(numArr[i + 1]);
-    i++
+//console.log(numArr)
+const test = [];
+for (let i = 0; i < numArr.length; i++) {
+  newNumArr.push(numArr[i]);
+  const subArr = []
+  subArr.push(numArr[i]);
+  let bool = true;
+  while (bool === true) {
+    if (numArr[i + 1]?.[1] - 1 !== undefined && numArr[i][1] === numArr[i + 1]?.[1] - 1) {
+      newNumArr.push(numArr[i + 1]);
+      subArr.push(numArr[i + 1]);
+      i++
+  } else {
+    bool = false;
+    test.push(subArr);
+  }
 //todo: finish number groupings
   }  
 }
+const finalArr = [];
+console.log(newNumArr);
+for (let i = 0; i < test.length; i++) {
+  const bool = test[i].some(x => {
+    if (arrData?.[x[0] -1 ]?.[x[1]]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0] -1 ]?.[x[1]] !== undefined){
+    //console.log(arrData?.[x[0] -1 ]?.[x[1]]?.match(/[\*=@&\/#%\+\$]/))
+      return true;
+    }
+    else if (arrData?.[x[0] - 1]?.[x[1] + 1]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0] - 1]?.[x[1] + 1] !== undefined){
+      //console.log(arrData?.[x[0] - 1]?.[x[1] + 1]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    }
+    else if (arrData?.[x[0] + 1]?.[x[1]]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0] + 1]?.[x[1]] !== undefined){
+      //console.log(arrData?.[x[0] + 1]?.[x[1]]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    }
+    else if (arrData?.[x[0] + 1]?.[x[1] +1]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0] + 1]?.[x[1] +1] !== undefined){
+      //console.log(arrData?.[x[0] + 1]?.[x[1] +1]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    }
+    else if (arrData?.[x[0] - 1]?.[x[1] - 1]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0] - 1]?.[x[1] - 1] !== undefined){
+      //console.log(arrData?.[x[0] - 1]?.[x[1] - 1]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    }
+    else if (arrData?.[x[0]]?.[x[1] + 1]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0]]?.[x[1] + 1] !== undefined){
+      console.log(arrData?.[x[0]]?.[x[1] + 1]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    }
+    else if (arrData?.[x[0]]?.[x[1] - 1]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0]]?.[x[1] - 1] !== undefined){
+      console.log(arrData?.[x[0]]?.[x[1] - 1]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    }
+    else if (arrData?.[x[0] + 1]?.[x[1] - 1]?.match(/[\*=@&\/#%\+\$-]/) !== null && arrData?.[x[0] + 1]?.[x[1] - 1] !== undefined){
+      console.log(arrData?.[x[0] + 1]?.[x[1] - 1]?.match(/[\*=@&\/#%\+\$]/))
+      return true;  
+    } else return false;
+  })
+  if (bool === true) {
+    finalArr.push(test[i]);
+  }
+}
+console.log(finalArr);
+console.log(arrData[0 + 1][28]);
+const nums = [];
+finalArr.map(x => {
+  const arr = [];
+  const newArr = [];
+  x.map(j => {
+    arr.push(j[2]);
+  })
+  let num = arr.reduce((acc, x) => {
+    acc = acc + x;
+    return acc;
+  },'');
+  num = parseInt(num);
+  nums.push(num);
+});
+console.log(util.inspect(nums, { maxArrayLength: null }));
+const output = nums.reduce((acc, x) => {
+  return acc + x
+},0)
+console.log(output);
+
 
 
 
