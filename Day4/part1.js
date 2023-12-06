86d427f0d1bbb4f62194cd30abe87a9d10ffc514
AmbitionSZN <jahlwalk22635@gmail.com>
2023-12-06 17:29:33 -0500

    finished day4 part1

--- Day4/part1.js
+++ Day4/part1.js
@@ -8,24 +8,34 @@ const data = fs.readFileSync('puzzleinput.txt', 'utf8', (err, data) => {
 });
 const arrData = data.split(/\n/);
 
-const matches = [];
-arrData.map(x => {
+//stores the point total of each game
+const gameScores = arrData.map(x => {
   const array = x.replace(/.*: /, '');
   const arr = array.split(/ \| |:/);
   const numArr = [];
-  //console.log(arr)
   arr.map(x => {
     const num = x.match(/[0-9][0-9]|[0-9]/g);
-    numArr.push(num);
-    
-  })
-  console.log(numArr[0]);
-  for (let i = 0; i < numArr[0].length; i++) {
-    for (let j = 0; j < numArr[1].length; j++) {
-      if (numArr[0][i] === numArr[1][j]) {
-        matches.push((x[0][i]));
+    numArr.push(num); 
+  });
+  const matches =  numArr[0].filter(x => {
+    for (let i = 0; i < numArr[1].length; i++) {
+      if (x === numArr[1][i]) {
+        return true;
       }
     }
+  });
+  let score = 0;
+  for (let i = 0; i < matches.length; i++) {
+    if (typeof matches[i] === 'string') {
+        let num = parseInt(matches[i]);
+        if (i >= 1) {
+          score = score * 2;
+        } else score++;
+    }
   }
-  //console.log(matches);
+  return score;
 });
+scoreSum = gameScores.reduce((acc, x) => {
+  return acc += x;
+},0);
+console.log(scoreSum);
