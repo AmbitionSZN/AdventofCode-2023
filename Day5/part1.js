36c512ed35743ac72ad4dcc56a6d9996631babb4
AmbitionSZN <jahlwalk22635@gmail.com>
2023-12-09 01:53:37 -0500

    more progress on day5 part1

--- Day5/part1.js
+++ Day5/part1.js
@@ -12,12 +12,15 @@ const arrData = data.split(/\r\n\r\n/);
 
 const almanac = arrData.map(x => {
   const arr = x.split(/:\r\n|:/);
-  console.log(arr);
+  //console.log(arr);
   arr[1] = arr[1].split(/\r\n/);
-  //arr[1].shift();
+  //console.log(arr);
   for (let i = 0; i < arr[1].length; i++) {
-    arr[1][i] = parseInt(arr[1][i]);
+    arr[1][i] = arr[1][i].split(/ /);
+    for (let j = 0; j < arr[1][i].length; j++) {
+      arr[1][i][j] = parseInt(arr[1][i][j]);
+    }
   }
   return arr;
 })
-//console.log(almanac);
+console.log(almanac[1]);
