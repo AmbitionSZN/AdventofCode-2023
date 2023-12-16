5b692f86b19ae9d63dcf67651ce958a28555a479
AmbitionSZN <jahlwalk22635@gmail.com>
2023-12-15 21:46:58 -0500

    progress on day9 part1

--- Day9/part1.js
+++ Day9/part1.js
@@ -18,20 +18,80 @@ const arrData = (() => {
   })
 })();
 
-const test = [];
 
-function predictor (history) {
+
+
+function predictor (history, pred) {
+  const newArr = [...history];
   const arr = [];
-  for (let i = 0; i < history.length - 1; i++) {
-    arr.push(history[i] - history[i + 1]);
+  const predicted = [pred]
+  if (predicted[0] === undefined) {
+    predicted[0] = [...history];
+  }
+  for (let i = 0; i < newArr.length - 1; i++) {
+    arr.push(newArr[i] - newArr[i + 1]);
   }
   test.push(arr);
-  if (arr.every(x => x === 0)) {
+  predicted.push(arr);
 
+  if (arr.every(x => x === 0)) {
+    return predicted;
   }
-  console.log(arr);
-  predictor(arr);
+
+  return predictor(arr, predicted);
 }
 
-predictor(arrData[0]);
+const output = arrData.map(x => {
+  const test = [];
+  function predictor (history, pred) {
+    const newArr = [...history];
+    const arr = [];
+    const predicted = [pred]
+    if (predicted[0] === undefined) {
+      predicted[0] = [...history];
+    }
+    for (let i = 0; i < newArr.length - 1; i++) {
+      arr.push(newArr[i + 1] - newArr[i]);
+    }
+    test.push(arr);
+    predicted.push(arr);
+  
+    if (arr.every(x => x === 0)) {
+      return predicted;
+    }
+    return predictor(arr, predicted);
+  }
+  
+  predictor(x);
+  //console.log(test);
+  test.reverse();
+  const vals = []
+  const extrapolated = test.reduce((acc, y) => {
+    acc = acc + y[y.length - 1];
+    vals.push(acc);
+
+    return acc;
+  },0)
+  return vals;
+})
+
+console.log(output);
+const final = output.map(x => {
+  return x.reduce((acc, y) => {
+    return acc += acc + y
+  },0)
+})
+const finalFinal = final.reduce((acc, x) => {
+  return acc += x;
+},0)
+
+console.log(finalFinal);
+
+
+
+
+
+
+
+
 
